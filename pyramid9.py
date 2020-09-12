2f2075bce23e9b40d6d8d09a622c392f62b505ea
Anubhav Singh <66737188+4nu6h4v51ngh@users.noreply.github.com>
2020-09-12 17:28:16 +0530

    Add files via upload

---
+++ pyramid9.py
@@ -0,0 +1,14 @@
+row=int(input("enter the no of rows"))
+i=1
+while i<row:
+	space=row-i-1
+	while space>0:
+		print(end="  ")
+		space=space-1
+	num=i
+	j=1
+	while j<=num:
+		print(j,end=" ")
+		j=j+1
+	i=i+1
+	print('\n')
